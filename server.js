e52d59a20011ce0867567a7dacda9dbcd7f2ba59
Chidori <business.faissal@outlook.com>
2022-05-12 01:03:55 -0500

    refactored codebase

--- server.js
+++ server.js
@@ -2,24 +2,18 @@
 const express = require(`express`);
 
 // creates the server and sets the PORT
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 // routes for api and html
-const apiRoutes = require(`./Routes/apiRoutes`);
-const htmlRoutes = require(`./Routes/htmlRoutes`);
+const apiRoutes = require(`./routes/apiRoutes`);
+const htmlRoutes = require(`./routes/htmlRoutes`);
 
-
-// can use this for deleted feature
-app.use(express.urlencoded({ extended: true }));
 // middleware
 app.use(express.json());
 app.use(express.static(`public`));
-
+// can use this for deleted feature
+app.use(express.urlencoded({ extended: true }));
 app.use(`/api`, apiRoutes);
 app.use(`/`, htmlRoutes);
 
-
-
-app.listen(PORT, () => {
-  console.log(`API server now on PORT ${PORT}`);
-});
+app.listen(PORT, () => console.log(`API server now on PORT ${PORT}`));
