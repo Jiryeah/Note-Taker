cb06cdf3824a727b8e6413e61013a6ee16121fe3
Chidori <business.faissal@outlook.com>
2022-05-12 22:30:17 -0500

    fixed file path

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const htmlRoutes = require(`./routes/htmlRoutes`);
 app.use(express.json());
 app.use(express.static(`public`));
 // can use this for deleted feature
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(`/api`, apiRoutes);
 app.use(`/`, htmlRoutes);
 
